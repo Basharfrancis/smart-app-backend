2216a634cc8a8a38c9a1726ce2b49f1f8342ad76
Bashar Francis <bfrancis@nuundigital.com>
2020-03-29 21:24:22 -0600

    add database online

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controller/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-rectangular-68936',
     user : 'frank',
     password : '12345',
     database : 'smartbrain'
